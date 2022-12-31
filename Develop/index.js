efe8ec55b269c6321c9bf15eb3cd63cb537a592a
kassrojas <kassandrarojass@gmail.com>
2022-12-31 14:45:59 -0500

    added criteria set up some promptsg

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,26 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+
+const prompt = inquirer.createPromptModule();
+
+prompt([
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is your id?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+]).then((input) => {
+    console.log(input);
+});
