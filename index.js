9618efa60bf675de5997652fbd85de8206c52291
kassrojas <kassandrarojass@gmail.com>
2022-12-31 15:26:25 -0500

    used template html deleted other html

--- index.js
+++ index.js
@@ -7,17 +7,14 @@ const prompt = inquirer.createPromptModule();
 
 prompt([
     {
-        type: 'input',
         message: 'What is your name?',
         name: 'name',
     },
     {
-        type: 'input',
         message: 'What is your id?',
         name: 'id',
     },
     {
-        type: 'input',
         message: 'What is your email?',
         name: 'email',
     },
