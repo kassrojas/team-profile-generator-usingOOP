44f9140628775c19c6bab955a64ceb5f66711d53
kassrojas <kassandrarojass@gmail.com>
2023-01-02 20:59:39 -0500

    adding questions and prompt modules

--- Develop/index.js
+++ Develop/index.js
@@ -1,35 +1,96 @@
 const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+// const Engineer = require('./lib/Engineer');
+// const Intern = require('./lib/Intern');
+// const generateTeamMembers = require('./src/page-template');
 const inquirer = require('inquirer');
+const prompt = inquirer.createPromptModule();
 const fs = require('fs');
 
-const prompt = inquirer.createPromptModule();
+// set empty array to add newStaff into
+const newStaff = [];
 
-const writeToFile = ({ html }) => {
-    try {
-        fs.writeFileSync(html);
-        console.log('Success!');
-    } catch (err) {
-        console.log(err);
+// initial questions for all employees
+const questions = async () => {
+    const input = await inquirer
+    .prompt([
+        {
+            message: 'What is your name?',
+            name: 'name',
+        },
+        {
+            message: 'What is your ID number?',
+            name: 'id',
+        },
+        {
+            message: 'What is your email?',
+            name: 'email',
+        },
+        {
+            type: 'list',
+            message: 'What is your current role?',
+            choices: ["Manager", "Engineer", "Intern"],
+            name: 'role',
+        }
+    ])
+    
+    // if manager is selected
+    if (input.role === 'Manager'){
+        const managerInput = await inquirer
+        .prompt ([
+            {
+                message: 'What is your office number?',
+                name: 'officeNumber',
+            },
+        ])
+    
+        const newManager = new Manager(
+            input.name,
+            input.id, 
+            input.email,
+            input.officeNumber, 
+        );
+        
+        //adds newManager to newStaff array
+        newStaff.push(newManager);
     }
-};
-
-
-prompt([
-    {
-        message: 'What is your name?',
-        name: 'name',
-    },
-    {
-        message: 'What is your id?',
-        name: 'id',
-    },
-    {
-        message: 'What is your email?',
-        name: 'email',
-    },
-])
-    .then(generateHTML)
-    .then(writeToFile)
-    .catch(err => console.log(err));
+    // if manager is selected
+    if (input.role === 'Manager'){
+        const managerInput = await inquirer
+        .prompt ([
+            {
+                message: 'What is your office number?',
+                name: 'officeNumber',
+            },
+        ])
+    
+        const newManager = new Manager(
+            input.name,
+            input.id, 
+            input.email,
+            input.officeNumber, 
+        );
+        
+        //adds newManager to newStaff array
+        newStaff.push(newManager);
+    }
+    // if manager is selected
+    if (input.role === 'Manager'){
+        const managerInput = await inquirer
+        .prompt ([
+            {
+                message: 'What is your office number?',
+                name: 'officeNumber',
+            },
+        ])
+    
+        const newManager = new Manager(
+            input.name,
+            input.id, 
+            input.email,
+            input.officeNumber, 
+        );
+        
+        //adds newManager to newStaff array
+        newStaff.push(newManager);
+    }
+}
