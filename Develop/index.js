4eb6c3ac42d61e50b87e29c04a687c0a99170243
kassrojas <kassandrarojass@gmail.com>
2023-01-02 20:37:02 -0500

    moved template literal from index.js to page-template.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,6 @@
-// const Manager = require('./lib/Manager');
-// const Engineer = require('./lib/Engineer');
-// const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
@@ -15,57 +15,6 @@ const writeToFile = ({ html }) => {
     }
 };
 
-const generateHTML = (input) => {
-    return {
-        html: `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
-    <title>Team Generator</title>
-    
-</head>
-<body>
-    <header>
-    <h1>My Team</h1>
-    </header>
-    <main>
-    <article>
-        <h2>Manager</h2>
-        <ul>
-        <li>ID: ${input.id}</li>
-        <li>Email: <a href="${input.email}">${input.email}</a></li>
-        <li>Office Number: ${input.officeNum}</li>
-        </ul>
-    </article>
-    <article>
-        <h2>Engineer</h2>
-        <ul>
-        <li>ID: ${input.id}</li>
-        <li>Email: <a href="${input.email}">${input.email}</a></li>
-        <li>GitHub: <a href="https://www.github.com/${input.github}">${input.github}</a></li>
-        </ul>
-    </article>
-    <article>
-        <h2>Intern</h2>
-        <ul>
-        <li>ID: ${input.id}</li>
-        <li>Email: <a href="#${input.email}">${input.email}</a></li>
-        <li>School: ${input.school}</li>
-        </ul>
-    </article>
-    </main>
-    <footer>
-    &copy; 2022-2023
-    </footer>
-</body>
-</html>
-    `
-    };
-};
 
 prompt([
     {
--- Develop/src/page-template.js
+++ Develop/src/page-template.js
@@ -5,5 +5,51 @@ const generateTeamMembers = (team) => {
 
 // export function to generate entire page
 module.exports = (team) => {
-    // TODO: MAIN HTML TEMPLATE LITERAL GOES HERE
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    <title>Team Generator</title>
+    
+</head>
+<body>
+    <header>
+    <h1>My Team</h1>
+    </header>
+    <main>
+    <article>
+        <h2>Manager</h2>
+        <ul>
+        <li>ID: ${input.id}</li>
+        <li>Email: <a href="${input.email}">${input.email}</a></li>
+        <li>Office Number: ${input.officeNum}</li>
+        </ul>
+    </article>
+    <article>
+        <h2>Engineer</h2>
+        <ul>
+        <li>ID: ${input.id}</li>
+        <li>Email: <a href="${input.email}">${input.email}</a></li>
+        <li>GitHub: <a href="https://www.github.com/${input.github}">${input.github}</a></li>
+        </ul>
+    </article>
+    <article>
+        <h2>Intern</h2>
+        <ul>
+        <li>ID: ${input.id}</li>
+        <li>Email: <a href="#${input.email}">${input.email}</a></li>
+        <li>School: ${input.school}</li>
+        </ul>
+    </article>
+    </main>
+    <footer>
+    &copy; 2022-2023
+    </footer>
+</body>
+</html>
+    `
 };
