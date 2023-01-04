ddb4117bea7a62f4d4cd010a3ddd35052d3a07a7
kassrojas <kassandrarojass@gmail.com>
2023-01-03 19:48:44 -0500

    added some tests for employee

--- Develop/__tests__/Employee.test.js
+++ Develop/__tests__/Employee.test.js
@@ -1,5 +1,19 @@
 const Employee = require("../lib/Employee");
 
 describe("Employee", () => {
+    describe ("instantiate Employee", () => {
+        it ("should take in Employee as object", () => {
+            
+            const employee = new Employee();
+            
+            expect (typeof(employee)).toBe("object");
+        });
+        
+        it ("should set name using constructor arguments", () =>{
+            const name = "Kass";
+            const employee = new Employee(name);
+            expect (employee.name).toBe(name);
+        });
+    })
 
 });
